68cd58d94859640055d1888577b1bfc054978d60
torsami <torsami77@gmail.com>
2020-05-04 16:44:58 +0100

    chore(setup react): setup and integrate create-react-app templates
    - setup react environment
    - avails react-component template
    - creates test directories
    - configures eslint and prettierrc definitions
    - establishes gh-pages as test deploy environment
    
    [Delivers]

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  "extends": [
+    "react-app",
+    "airbnb",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+    "prettier/react"
+  ],
+  "plugins": [
+    "jsx-a11y",
+    "prettier"
+  ],
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "prettier/prettier": [
+      "error", {
+      "semi": true
+      }
+    ]
+  }
+};
