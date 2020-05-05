004e11437f454e3b7c7e23cc104cacddf23dfe67
torsami <torsami77@gmail.com>
2020-05-05 15:29:01 +0100

    check-styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,22 +5,14 @@ module.exports = {
     node: true,
     jest: true
   },
-  "extends": [
-    "react-app",
-    "airbnb",
-    "plugin:jsx-a11y/recommended",
-    "prettier",
-    "prettier/react"
-  ],
-  "plugins": [
-    "jsx-a11y",
-    "prettier"
-  ],
-  "rules": {
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "prettier/prettier": [
-      "error", {
-      "semi": true
+  extends: ['react-app', 'airbnb', 'plugin:jsx-a11y/recommended', 'prettier', 'prettier/react'],
+  plugins: ['jsx-a11y', 'prettier', 'stylelint-prettier'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true
       }
     ]
   }
